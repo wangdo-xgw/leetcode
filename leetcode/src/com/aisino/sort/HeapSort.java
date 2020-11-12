cee45ceac2fd2b7e56953719a1f1fe23a7cf896f
wangdo-xgw <73640768+wangdo-xgw@users.noreply.github.com>
2020-11-12 08:58:52 +0800

    vcs.xml和workspace.xml

--- leetcode/src/com/aisino/sort/HeapSort.java
+++ leetcode/src/com/aisino/sort/HeapSort.java
@@ -2,7 +2,13 @@ package com.aisino.sort;
 
 /**
  * @author: xuguangwu
- * @Description:
+ * https://www.cnblogs.com/chengxiao/p/6129630.html
+ * 堆排序是利用堆这种数据结构而设计的一种排序算法，堆排序是一种选择排序。
+ * 它的最坏，最好，平均时间复杂度均为O(nlogn)，它也是不稳定排序。
+ *
+ * 该数组从逻辑上讲就是一个堆结构，我们用简单的公式来描述一下堆的定义就是：
+ * 大顶堆：arr[i] >= arr[2i+1] && arr[i] >= arr[2i+2]
+ * 小顶堆：arr[i] <= arr[2i+1] && arr[i] <= arr[2i+2]
  * @date Created in 2020-10-29-下午 5:42
  */
 public class HeapSort {
